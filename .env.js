310858c20746441d6f0f261791dfb06cbbe8f04b
Stephen Howe <Stevehowe10@gmail.com>
2019-10-01 17:09:35 -0600

    api call working with user input. Not able to default to correct song yet

--- .env.js
+++
@@ -1,5 +0,0 @@
-// # Spotify API keys
-console.log("env is working")
-
-SPOTIFY_ID=ddac07077c194cac8902b5e66555c410
-SPOTIFY_SECRET=e5fbdd7577c1435ebaf77dc747345b44
--- liri.js
+++ liri.js
@@ -13,11 +13,43 @@ var keys = require("./keys");
 // Initialize the spotify API client with keys
 var spotify = new Spotify(keys.spotify);
 
- spotify.search({ type: 'track', query: 'The Sign' }, function(err, data) {
-     if (err) {
-      return console.log('Error occurred: ' + err);
+var callAPI = function(songName) {
+    if (songName === undefined) {
+      songName = "The Sign";
     }
-   
-  console.log(data); 
-   });
+    spotify.search(
+      {
+        type: "track",
+        query: songName,
+        limit: 3
+      },
+      function(err, data) {
+        if (err) {
+          console.log("Error occurred: " + err);
+          return;
+        }
+        var songs = data.tracks.items;
+        for (var i = 0; i < songs.length; i++) {
+          console.log("Artist: " + songs[i].artists[0].name);
+          console.log("Song: " + songs[i].name);         
+          console.log("Song Preview: " + songs[i].preview_url);
+          console.log("Album: " + songs[i].album.name);                   
+          console.log("----------------------------------------------------");
+        }
+      }
+    );
+  };
 
+  var userinput = function(caseData, functionData){
+      switch (caseData) {
+          case "spotify-this-song":
+          callAPI(functionData);
+          break;
+      }
+  }
+  var command = function(targetAPI, pickSong) {
+    userinput(targetAPI, pickSong);
+  }
+  
+    command(process.argv[2], process.argv[3]);
+  
