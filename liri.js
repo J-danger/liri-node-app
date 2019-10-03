edeff595049fab135883638356b35011f82803ab
Stephen Howe <Stevehowe10@gmail.com>
2019-10-03 17:04:49 -0600

    Spotify API working with dynamic inputs. Concert API not working

--- liri.js
+++ liri.js
@@ -1,26 +1,25 @@
-
-
-
-
-
-
 require("dotenv").config();
 // Import the node-spotify-api NPM package.
 var Spotify = require("node-spotify-api");
 // Import the API keys
 var keys = require("./keys");
+let request = require("request");
 
 // Initialize the spotify API client with keys
 var spotify = new Spotify(keys.spotify);
 
-var callAPI = function(songName) {
-    if (songName === undefined) {
-      songName = "The Sign";
+let userInput = process.argv[2];
+let userQuery = process.argv.slice(3).join(" ");
+
+// spotify API call
+var spotifySong = function() {
+    if (userQuery === undefined) {
+      userQuery = "The Sign";
     }
     spotify.search(
       {
         type: "track",
-        query: songName,
+        query: userQuery,
         limit: 3
       },
       function(err, data) {
@@ -40,16 +39,41 @@ var callAPI = function(songName) {
     );
   };
 
-  var userinput = function(caseData, functionData){
-      switch (caseData) {
+  function concertThis(){
+    
+    var queryURL = "https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp"
+
+    request(queryURL, function(response){
+      let array = JSON.parse();
+    console.log("Venue: " + array.data[0].venue.name)
+    if (region === ""){
+      console.log("Location" + array.data[0].venue.city + "," + array.data.venue.country)
+    } else {
+      console.log("Location" + city + "," + region)
+    }
+    var eventDate = moment(array.data[0].datetime).format('MM/DD/YYYY');
+    console.log("Date of the Event:", eventDate);
+
+  })
+}
+
+
+
+// user input 
+  var userinput = function(userInput, userQuery){
+      switch (userInput) {
           case "spotify-this-song":
-          callAPI(functionData);
+            spotifySong();
+          break;
+          case "concert-this":
+            concertThis();
           break;
       }
   }
-  var command = function(targetAPI, pickSong) {
-    userinput(targetAPI, pickSong);
-  }
+
+  userinput(userInput, userQuery)
+  // var command = function(targetAPI, pickSong) {
+  //   userinput(targetAPI, pickSong);
+  // }
   
-    command(process.argv[2], process.argv[3]);
-  
+  //   command(process.argv[2], process.argv[3]);
