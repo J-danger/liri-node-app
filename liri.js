5b749ac89f8e33f51a83a4de32db99584a97b16a
Stephen Howe <Stevehowe10@gmail.com>
2019-10-03 18:14:24 -0600

    cleaned code

--- liri.js
+++ liri.js
@@ -1,18 +1,13 @@
 require("dotenv").config();
-// Import the node-spotify-api NPM package.
 var Spotify = require("node-spotify-api");
-// Import the API keys
 var keys = require("./keys");
-
 var axios = require("axios");
 var fs = require("fs");
-// Initialize the spotify API client with keys
 var spotify = new Spotify(keys.spotify);
-
 let userInput = process.argv[2];
 let userQuery = process.argv.slice(3).join(" ");
 
-// spotify API call
+
 var spotifySong = function() {
     if (userQuery === undefined) {
       userQuery = "The Sign";
@@ -40,34 +35,32 @@ var spotifySong = function() {
     );
   };
 
-  function concertThis(){
-    
-    var concertURL = "https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp"
+function concertThis(){
+  
+  var concertURL = "https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=codingbootcamp"
 
-    axios.get(concertURL).then(function (response){
-      // var array = JSON.stringify(response)      
-          console.log(response.data[1].venue.name)
-          console.log(response.data[1].venue.city)
-          var date = (response.data[1].datetime)
-          console.log(date)
-          // console.log("Location" + response.data[0].venue.city + "," + response.data.venue.country)
-    })  
+  axios.get(concertURL).then(function (response){           
+        console.log(response.data[1].venue.name)
+        console.log(response.data[1].venue.city)
+        var date = (response.data[1].datetime)
+        console.log(date)          
+  })  
 }
 
 function omdbAPI(){
   if (userQuery === undefined) {
     userQuery = "Mr. nobody";
   };
-  var omdbURL = "http://www.omdbapi.com/?t=" + userQuery + "&apikey=2c69a114"
-  axios.get(omdbURL).then(function (response){
-    console.log(response.data.Title)
-    console.log(response.data.Released)
-    console.log(response.data.imdbRating)
-    console.log(response.data.Ratings[1])
-    console.log(response.data.Language)
-    console.log(response.data.Plot)
-    console.log(response.data.Actors)
-})
+    var omdbURL = "http://www.omdbapi.com/?t=" + userQuery + "&apikey=2c69a114"
+    axios.get(omdbURL).then(function (response){
+      console.log(response.data.Title)
+      console.log(response.data.Released)
+      console.log(response.data.imdbRating)
+      console.log(response.data.Ratings[1])
+      console.log(response.data.Language)
+      console.log(response.data.Plot)
+      console.log(response.data.Actors)
+  })
 }
 
 function doWhat() { 
@@ -82,27 +75,24 @@ function doWhat() {
   });
 };
 
+var userinput = function(userInput){
+    switch (userInput) {
+        case "spotify-this-song":
+          spotifySong();
+        break;
+        case "concert-this":
+          concertThis();
+        break;
+        case "movie-this":
+          omdbAPI();
+        break;
+        case "do-what-it-says":
+          doWhat();
+        break;
 
-
-// user input 
-  var userinput = function(userInput){
-      switch (userInput) {
-          case "spotify-this-song":
-            spotifySong();
-          break;
-          case "concert-this":
-            concertThis();
-          break;
-          case "movie-this":
-            omdbAPI();
-          break;
-          case "do-what-it-says":
-            doWhat();
-          break;
-
-          default:
-    console.log(`I'm sorry!`)
+        default:
+  console.log(`I'm sorry!`)
+  }
 }
-      }
   userinput(userInput, userQuery)
  
